97d7df52b90dec5e4a54b73366122b397f72709d
Ghada Bennasr <gh.bennasr@gmail.com>
2024-05-01 20:40:47 +0200

    instructions page

--- src/pages/InstructionPage.jsx
+++ src/pages/InstructionPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function InstructionPage() {
-  return <div>InstructionPage</div>;
+  return (
+    <div>
+      "Get ready to click, laugh, and share! Our group project brings you a
+      real-time survey experience built with React JS and a live API. As you
+      answer, we'll capture and project data for instant enjoyment. Let’s make
+      this interactive session a highlight of our cohort!"
+    </div>
+  );
 }
 
 export default InstructionPage;
