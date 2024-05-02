c5a8c351463060e5713aa0e4b1d3ccd7c1cfdcd3
ecemonkol <ecemonkol@gmail.com>
2024-05-02 12:29:55 +0200

    add start button to Instruction Page

--- src/pages/InstructionPage.jsx
+++ src/pages/InstructionPage.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function InstructionPage() {
   return (
     <div>
-      "Get ready to click, laugh, and share! Our group project brings you a
-      real-time survey experience built with React JS and a live API. As you
-      answer, we'll capture and project data for instant enjoyment. Let’s make
-      this interactive session a highlight of our cohort!"
+      <div>
+        "Get ready to click, laugh, and share! Our group project brings you a
+        real-time survey experience built with React JS and a live API. As you
+        answer, we'll capture and project data for instant enjoyment. Let’s make
+        this interactive session a highlight of our cohort!"
+      </div>
+      <Link to="/1">
+        <button>START</button>
+      </Link>
     </div>
   );
 }
