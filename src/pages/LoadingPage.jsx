8672fbb42a1f8faf1f39200ce9895f8ab1aedf75
Ghada Bennasr <gh.bennasr@gmail.com>
2024-05-03 01:54:40 +0200

    loading-page-advice

--- src/pages/LoadingPage.jsx
+++ src/pages/LoadingPage.jsx
@@ -1,9 +1,29 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-// const API_URL =
-function Loading() {
-  return <div>Loading</div>;
+const API_URL = "https://api.adviceslip.com/advice";
+function LoadingPage() {
+  const [advice, setAdvice] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(API_URL);
+        console.log(response.data);
+        setAdvice(response.data.slip.advice); // Extract advice from the response object
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h3>Random Advice:</h3>
+      <p>{advice}</p>
+    </div>
+  );
 }
 
-export default Loading;
+export default LoadingPage;
