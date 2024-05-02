b0626ffc37cf1ffb751a58d6fe02d6e438d8b47d
Tetiana Kanafotska <tanyakanf@gmail.com>
2024-05-02 20:09:00 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customPink: "#FF97CA",
+        customPinkHover: "#FF78AB",
+        customGreen: "#1FCF6A",
+        customGreenHover: "#19AB5B",
+      },
+    },
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "5.052rem",
+    },
   },
   plugins: [],
-}
-
+};
