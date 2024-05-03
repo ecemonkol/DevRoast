92d3c417acb9e44a996cf95a9b6310226d8690d3
tanyakanf <tanyakanf@gmail.com>
2024-05-04 00:16:24 +0200

    cleanup

--- src/pages/ResultsPage.jsx
+++ src/pages/ResultsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios, { all } from "axios";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 
 const URLanswers = "https://questions-server.adaptable.app/answers";
