1e1abc408240af2fffae616e85e062e7c70935ec
Tetiana Kanafotska <tanyakanf@gmail.com>
2024-05-03 16:31:32 +0200

    Update ChooseMode.jsx

--- src/pages/ChooseMode.jsx
+++ src/pages/ChooseMode.jsx
@@ -5,7 +5,7 @@ function ChooseMode() {
   const navigate = useNavigate();
   const handleClick = (e) => {
     const type = e.target.value;
-    navigate(`/instructions/${type}`);
+    navigate("/choose-avatar");
   };
   return (
     <div className="flex flex-col items-center justify-center h-screen space-grotesk">
